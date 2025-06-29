3c6b3c20551ea538979209f0615169a3e0831ad0
taiyakihitotsu <taiyakihitotsu@proton.me>
2025-06-29 11:47:08 +0900

    wip: completed fn

--- src/builtins.ts
+++ src/builtins.ts
@@ -32,38 +32,47 @@ export type N2 = N1 | 2
 export type N3 = N2 | 3
 export type N4 = N3 | 4
 
+export type ExpandPre<narg extends N4> = (narg extends N0 ? '' : narg extends N1 ? ([string] | readonly [string]) : narg extends N2 ? ([string, string] | readonly [string, string]) : ([string, string, string] | readonly [string, string, string])) | string
+
+export type t4 = string | [string] | readonly [string] |
+[string, string] | readonly [string, string] |
+[string, string, string] | readonly [string, string, string] |
+[string, string, string, string] | readonly [string, string, string, string]
+
 // [note]
 // this is from v0.3.0
 // if you want to check the unroll version, see there.
-export const _macro = <
+// [note]
+// this is named `fn` currently but I'll be changed.
+export const fn = <
   SexprFunction extends string
-, DefaultPre extends [string, string] | readonly [string, string] | string
+, DefaultPre extends t4
 , narg extends N4
 >(num: narg) =>
 <a0, a1, a2, a3, ret>
-(f: narg extends N0 ? () => ret : narg extends N1 ? (w: a0) => ret : narg extends N2 ? (w: a0, x:a1) => ret : narg extends N3 ? (w: a0, x:a1, y:a2) => ret : (w: a0, x:a1, y:a2, z:a3) => ret) =>
-<
-  Pre extends [string, string] | readonly [string, string] | string = pre.add
+(f: narg extends N0 ? () => ret : narg extends N1 ? (w: a0) => ret : narg extends N2 ? (w: a0, x:a1) => ret : narg extends N3 ? (w: a0, x:a1, y:a2) => ret : (w: a0, x:a1, y:a2, z:a3) => ret) => <
+Pre extends t4 = DefaultPre
 >() =>
 < Arg0 extends FoxWith<a0, Arg0>
 , Arg1 extends FoxWith<a1, Arg1>
 , Arg2 extends FoxWith<a2, Arg2>
 , Arg3 extends FoxWith<a3, Arg3>
-, UnrollArgsStr extends `${narg extends N0 ? '' : ` ${FS<Arg0[c.SexprKey]>}`}${narg extends N1 ? '' :  ` ${FS<Arg1[c.SexprKey]>}`}${narg extends N2 ? '' :  ` ${FS<Arg2[c.SexprKey]>}`}${narg extends N3 ? '' :  ` ${FS<Arg3[c.SexprKey]>}`}`
-, SexprR extends Cion.Lisp<`(${SexprFunction}${narg extends N0 ? '' : ` ${FS<Arg0[c.SexprKey]>}`}${narg extends N1 ? '' :  ` ${FS<Arg1[c.SexprKey]>}`}${narg extends N2 ? '' :  ` ${FS<Arg2[c.SexprKey]>}`}${narg extends N3 ? '' :  ` ${FS<Arg3[c.SexprKey]>}`})`>
-, Pre0 extends string = Pre extends string ? `(${Pre} [${ForceStr<Arg0[c.SexprKey]>} ${ForceStr<Arg0[c.SexprKey]>}])` : `(${Pre[0]} ${ForceStr<Arg0[c.SexprKey]>})`
-, Pre1 extends string = Pre extends string ? `(${Pre} [${ForceStr<Arg0[c.SexprKey]>} ${ForceStr<Arg1[c.SexprKey]>}])` : `(${Pre[1]} ${ForceStr<Arg1[c.SexprKey]>})`
-, Pre2 extends string = Pre extends string ? `(${Pre} [${ForceStr<Arg0[c.SexprKey]>} ${ForceStr<Arg2[c.SexprKey]>}])` : `(${Pre[2]} ${ForceStr<Arg2[c.SexprKey]>})`
-, Pre3 extends string = Pre extends string ? `(${Pre} [${ForceStr<Arg0[c.SexprKey]>} ${ForceStr<Arg3[c.SexprKey]>}])` : `(${Pre[3]} ${ForceStr<Arg3[c.SexprKey]>})`
-, Ret0 = Pre[0] extends '' ? Arg0 : (Cion.Lisp<Pre0> extends ('nil' | 'false') ? never : Arg0)
-, Ret1 = Pre[1] extends '' ? Arg1 : (Cion.Lisp<Pre1> extends ('nil' | 'false') ? never : Arg1), Ret2 = Pre[2] extends '' ? Arg2 : (Cion.Lisp<Pre2> extends ('nil' | 'false') ? never : Arg2)
-, Ret3 = Pre[3] extends '' ? Arg3 : (Cion.Lisp<Pre3> extends ('nil' | 'false') ? never : Arg3)>
+, UnrollArgsStr extends `${narg extends N0 ? '' : `${FS<Arg0[c.SexprKey]>}`}${narg extends N1 ? '' :  ` ${FS<Arg1[c.SexprKey]>}`}${narg extends N2 ? '' :  ` ${FS<Arg2[c.SexprKey]>}`}${narg extends N3 ? '' :  ` ${FS<Arg3[c.SexprKey]>}`}`
+, SexprR extends Cion.Lisp<`(${SexprFunction}${narg extends N0 ? '' : ` ${UnrollArgsStr}`})`>
+, Pre0 extends string = Pre extends string ? `(${Pre} [${UnrollArgsStr}])` : `(${Pre[0]} ${ForceStr<Arg0[c.SexprKey]>})`
+, Pre1 extends string = Pre extends string ? `(${Pre} [${UnrollArgsStr}])` : `(${Pre[1]} ${ForceStr<Arg1[c.SexprKey]>})`
+, Pre2 extends string = Pre extends string ? `(${Pre} [${UnrollArgsStr}])` : `(${Pre[2]} ${ForceStr<Arg2[c.SexprKey]>})`
+, Pre3 extends string = Pre extends string ? `(${Pre} [${UnrollArgsStr}])` : `(${Pre[3]} ${ForceStr<Arg3[c.SexprKey]>})`
+, Ret0 = narg extends N0 ? never : Pre[0] extends '' ? Arg0 : (Cion.Lisp<Pre0> extends ('nil' | 'false') ? never : Arg0)
+, Ret1 = narg extends N0 ? never : Pre[1] extends '' ? Arg1 : (Cion.Lisp<Pre1> extends ('nil' | 'false') ? never : Arg1)
+, Ret2 = narg extends N0 ? never : Pre[2] extends '' ? Arg2 : (Cion.Lisp<Pre2> extends ('nil' | 'false') ? never : Arg2)
+, Ret3 = narg extends N0 ? never : Pre[3] extends '' ? Arg3 : (Cion.Lisp<Pre3> extends ('nil' | 'false') ? never : Arg3)>
 ( w?: Arg0 extends Ret0 ? Arg0 : never
 , x?: Arg1 extends Ret1 ? Arg1 : never
 , y?: Arg2 extends Ret2 ? Arg2 : never
 , z?: Arg3 extends Ret3 ? Arg3 : never): {[c.SexprKey]: SexprR, [c.ValueKey]: ret} => rfoxposs<SexprR, ret>(num === 0 ? (f as () => ret)() : num === 1 ? (f as (w: a0) => ret)(w![c.ValueKey]) : num === 2 ? (f as (w: a0, x: a1) => ret)(w![c.ValueKey], x![c.ValueKey]) : num === 3 ? (f as (w: a0, x: a1, y: a2) => ret)(w![c.ValueKey], x![c.ValueKey], y![c.ValueKey]) : (f as (w: a0, x: a1, y: a2, z: a3) => ret)(w![c.ValueKey], x![c.ValueKey], y![c.ValueKey], z![c.ValueKey]))
 
-export const add = _macro<'+', pre.add, 2>(2)((n: number, m:number) => n + m)
+export const add = fn<'+', pre.add, 2>(2)((n: number, m:number) => n + m)
 
 // export const add = <
 //   Pre extends [string, string] | readonly [string, string] | string = pre.add
