1c390d4948ab1818b663665a5b27d3b7f477c3da
taiyakihitotsu <taiyakihitotsu@proton.me>
2025-07-20 02:42:09 +0900

    [update] ltc now recognize Fn

--- src/compiler.ts
+++ src/compiler.ts
@@ -9,7 +9,7 @@ import * as ut from './type-util'
 // - 3. Call this compiler to earn the corresponding TS type
 //
 // Finally, we get both: S-exprs at the Cion context & TS types at the TS context.
-
+//
 // Default S would be key, map, vec.
 // Not-builtin fn pattern is processed in ltCompiler.
 export type PrimToTS<
@@ -22,6 +22,8 @@ export type PrimToTS<
     : H extends '"' | `'`
       ? string
     : S extends Cion.Builtins
+      ? Function
+    : S extends `(fn [${infer _}`
       ? Function
     : S
   : S
@@ -69,7 +71,8 @@ export type ltcError2 = 'ltcError2'
 export type ltcError3 = 'ltcError3'
 export type ltcError4 = 'ltcError4'
 export type Error<S extends string> = {error: S}
-export type ltCompiler<
+export type ltCompiler<Parsed extends string[]> = _ltCompiler<Parsed>
+export type _ltCompiler<
   Parsed  extends string[]
 , Current extends StackCon = {state: 'vec', stack: []}
 , Stack   extends StackCon[] = []> =
@@ -78,27 +81,27 @@ export type ltCompiler<
   : Parsed extends [infer F extends string, ...infer Rest extends string[]]
     ? F extends '[' | '{'
       ? Current['state'] extends 'fn'
-        ? ltCompiler<Rest, StackPush<Current, F>, Stack>
-      : ltCompiler<Rest, {state: (F extends '[' ? 'vec' : 'map'), stack: []}, [Current, ...Stack]>
+        ? _ltCompiler<Rest, StackPush<Current, F>, Stack>
+      : _ltCompiler<Rest, {state: (F extends '[' ? 'vec' : 'map'), stack: []}, [Current, ...Stack]>
     : F extends '('
-      ? ltCompiler<Rest, {state: 'fn', stack: []}, [Current, ...Stack]>
+      ? _ltCompiler<Rest, {state: 'fn', stack: []}, [Current, ...Stack]>
     : F extends ')'
       ? Stack extends [ infer Last extends StackCon
                       , ...infer Tail extends StackCon[]]
-        ? ltCompiler<Rest, StackPush<Last, Function>, Tail>
+        ? _ltCompiler<Rest, StackPush<Last, Function>, Tail>
       : Error<ltcError1>
     : F extends ']' | '}'
       ? Current['state'] extends 'fn'
-        ? ltCompiler<Rest, StackPush<Current, F>, Stack>
+        ? _ltCompiler<Rest, StackPush<Current, F>, Stack>
       : Stack extends [ infer Last extends StackCon
                       , ...infer Tail extends StackCon[]]
         ? Current['state'] extends 'map'
-          ? ltCompiler<Rest, StackPush<Last, VtoLispM<Current['stack']>>, Tail>
+          ? _ltCompiler<Rest, StackPush<Last, VtoLispM<Current['stack']>>, Tail>
         : Current['state'] extends 'vec'
-          ? ltCompiler<Rest, StackPush<Last, Current['stack']>, Tail>
+          ? _ltCompiler<Rest, StackPush<Last, Current['stack']>, Tail>
         : Error<ltcError2>
       : Error<ltcError3>
-    : ltCompiler<Rest, StackPush<Current, PrimToTS<F>>, Stack>
+    : _ltCompiler<Rest, StackPush<Current, PrimToTS<F>>, Stack>
   : Error<ltcError4>
 
 export type BracketWrap<S extends string> = `[${S}]`
