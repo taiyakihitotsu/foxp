a41caeb5da054a04a1d4fc1e90e6002193503eac
taiyakihitotsu <taiyakihitotsu@proton.me>
2026-04-04 03:59:12 +0900

    [chore] npm to pnpm, typescript v6.0.2

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+git submodule sync --recursive
+git submodule update --init --recursive
 
-npm install && git submodule add -f -b main git@github.com:taiyakihitotsu/condscript.git submodules/scripts && git submodule add -f -b master git@github.com:taiyakihitotsu/condsmith.git submodules/condsmith && git submodule sync --recursive && git submodule init && git submodule update --init --recursive && cd submodules/condsmith && npm install
+pnpm install --frozen-lockfile
+
+cd submodules/condsmith && pnpm install --no-frozen-lockfile
+cd ../..
